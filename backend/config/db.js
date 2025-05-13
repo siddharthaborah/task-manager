c2c04ce6ecfbb9165349ef1909b2c867303540c8
Siddhartha Borah <siddhartha.borah@outlook.com>
2025-05-13 10:22:30 +0000

    setup mongodb

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI,{});
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.error('MongoDB connection error:', err);
+        process.exit(1); // Exit the process with failure
+    }
+};
+
+module.exports = connectDB;
+             
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const connectDB = require('./config/db');
 
 const app = express();
 
@@ -23,11 +24,22 @@ app.use(
         }
  ));
 
+
+ // Connect to the database
+connectDB();
+
+
  // Middleware to parse JSON requests
 app.use(express.json());
 
 
+
 // Route to serve the frontend application
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/tasks", taskRoutes);
+// app.use("/api/reports", reportRoutes);
 
 
 // Start the server
